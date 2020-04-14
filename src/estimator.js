e5c3ae1aeb31b7e990e426377a45ba0725015c4a
emmanuelOlamilekan <leoteeee@yahoo.com>
2020-04-14 12:41:41 +0100

    correction and update

--- src/estimator.js
+++ src/estimator.js
@@ -8,28 +8,28 @@ const normalizeDuration = (periodType, timeToElapse) => {
 			days = timeToElapse * 7;
             break;
         case 'months':
-            days = timeToElapse *30;  
+            days = timeToElapse * 30;  
             break;
         default:
-            days=0;
+            days = 0;
             break;
     }
 	return days;
-}
+};
 const getImpact = (data,multiplier) => {
     let impact = {};
     const currentlyInfected = data.reportedCases * multiplier;
-    const totalDays = normalizeDuration(data.periodType,data.timeToElapse);
+    const totalDays = normalizeDuration(data.periodType, data.timeToElapse);
     let factor = Math.trunc(totalDays/3);
     let infectionsByRequestedTime = Math.pow(2,factor);
     impact.infectionsByRequestedTime = infectionsByRequestedTime;
     impact.currentlyInfected = currentlyInfected;
     return impact;
-}
+};
 const covid19ImpactEstimator = (data) => {
     const impact = getImpact(data,10);
     const severeImpact = getImpact(data,50);
-	let resp = {data: data,impact:impact,severeImpact:severeImpact};
+	let resp = {data: data, impact:impact, severeImpact:severeImpact};
 	return resp;
-}
+};
 export default covid19ImpactEstimator;
