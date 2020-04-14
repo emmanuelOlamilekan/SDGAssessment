17e21eedc4174f79dd5b90af4f3ff343640764af
emmanuelOlamilekan <leoteeee@yahoo.com>
2020-04-14 16:52:19 +0100

    corrections and update

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,13 @@
 const normalizeDuration = (periodType, timeToElapse) => {
   let days = 0;
-  switch (periodType) {
-    case 'days':
-      days = timeToElapse;
-      break;
-    case 'weeks':
-      days = timeToElapse * 7;
-      break;
-    case 'months':
-      days = timeToElapse * 30;
-      break;
-    default:
-      days = 0;
-      break;
+  if (periodType === 'days') {
+    days = timeToElapse;
+  }
+  if (periodType === 'months') {
+    days = timeToElapse * 30;
+  }
+  if (periodType === 'weeks') {
+    days = timeToElapse * 7;
   }
   return days;
 };
@@ -20,7 +15,7 @@ const getImpact = (data, multiplier) => {
   const currentlyInfected = data.reportedCases * multiplier;
   const totalDays = normalizeDuration(data.periodType, data.timeToElapse);
   const factor = Math.trunc(totalDays / 3);
-  const infectionsByRequestedTime = 2 ** factor;
+  const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
   const impact = {
     data,
     infectionsByRequestedTime,
