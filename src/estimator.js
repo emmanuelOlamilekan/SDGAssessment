e47e921ba99a2585a9f5bdd301e4bf1d5fe1c355
emmanuelOlamilekan <leoteeee@yahoo.com>
2020-04-14 13:25:37 +0100

    correction and update

--- src/estimator.js
+++ src/estimator.js
@@ -1,35 +1,35 @@
 const normalizeDuration = (periodType, timeToElapse) => {
-    let days = 0;
-    switch (periodType) {
-        case 'days':
-            days = timeToElapse;
-            break;
-        case 'weeks':
-            days = timeToElapse * 7;
-            break;
-        case 'months':
-            days = timeToElapse * 30;  
-            break;
-        default:
-            days = 0;
-            break;
-    }
-    return days;
+  let days = 0;
+  switch (periodType) {
+		case 'days':
+			days = timeToElapse;
+			break;
+		case 'weeks':
+			days = timeToElapse * 7;
+			break;
+		case 'months':
+			days = timeToElapse * 30;  
+			break;
+		default:
+			days = 0;
+			break;
+	}
+	return days;
 };
 const getImpact = (data,multiplier) => {
-    let impact = {};
-    const currentlyInfected = data.reportedCases * multiplier;
-    const totalDays = normalizeDuration(data.periodType, data.timeToElapse);
-    let factor = Math.trunc(totalDays/3);
-    let infectionsByRequestedTime = Math.pow(2,factor);
-    impact.infectionsByRequestedTime = infectionsByRequestedTime;
-    impact.currentlyInfected = currentlyInfected;
-    return impact;
+	let impact = {};
+	const currentlyInfected = data.reportedCases * multiplier;
+	const totalDays = normalizeDuration(data.periodType, data.timeToElapse);
+	let factor = Math.trunc(totalDays/3);
+	let infectionsByRequestedTime = Math.pow(2,factor);
+	impact.infectionsByRequestedTime = infectionsByRequestedTime;
+	impact.currentlyInfected = currentlyInfected;
+	return impact;
 };
 const covid19ImpactEstimator = (data) => {
-    const impact = getImpact(data,10);
-    const severeImpact = getImpact(data,50);
-    let resp = {data: data, impact:impact, severeImpact:severeImpact};
-    return resp;
+	const impact = getImpact(data,10);
+	const severeImpact = getImpact(data,50);
+	let resp = { data: data, impact:impact, severeImpact:severeImpact };
+	return resp;
 };
 export default covid19ImpactEstimator;
