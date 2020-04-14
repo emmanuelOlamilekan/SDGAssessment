27f03c921863b43ad3d281c0f2e4124854e1c6ac
emmanuelOlamilekan <leoteeee@yahoo.com>
2020-04-14 16:31:31 +0100

    corrections and update

--- src/estimator.js
+++ src/estimator.js
@@ -1,35 +1,37 @@
 const normalizeDuration = (periodType, timeToElapse) => {
   let days = 0;
   switch (periodType) {
-		case 'days':
-			days = timeToElapse;
-			break;
-		case 'weeks':
-			days = timeToElapse * 7;
-			break;
-		case 'months':
-			days = timeToElapse * 30;  
-			break;
-		default:
-			days = 0;
-			break;
-	}
-	return days;
+    case 'days':
+      days = timeToElapse;
+      break;
+    case 'weeks':
+      days = timeToElapse * 7;
+      break;
+    case 'months':
+      days = timeToElapse * 30;
+      break;
+    default:
+      days = 0;
+      break;
+  }
+  return days;
 };
-const getImpact = (data,multiplier) => {
-	let impact = {};
-	const currentlyInfected = data.reportedCases * multiplier;
-	const totalDays = normalizeDuration(data.periodType, data.timeToElapse);
-	let factor = Math.trunc(totalDays/3);
-	let infectionsByRequestedTime = Math.pow(2,factor);
-	impact.infectionsByRequestedTime = infectionsByRequestedTime;
-	impact.currentlyInfected = currentlyInfected;
-	return impact;
+const getImpact = (data, multiplier) => {
+  const currentlyInfected = data.reportedCases * multiplier;
+  const totalDays = normalizeDuration(data.periodType, data.timeToElapse);
+  const factor = Math.trunc(totalDays / 3);
+  const infectionsByRequestedTime = 2 ** factor;
+  const impact = {
+    data,
+    infectionsByRequestedTime,
+    currentlyInfected
+  };
+  return impact;
 };
 const covid19ImpactEstimator = (data) => {
-	const impact = getImpact(data,10);
-	const severeImpact = getImpact(data,50);
-	let resp = { data: data, impact:impact, severeImpact:severeImpact };
-	return resp;
+  const impact = getImpact(data, 10);
+  const severeImpact = getImpact(data, 50);
+  const resp = { data, impact, severeImpact };
+  return resp;
 };
 export default covid19ImpactEstimator;
